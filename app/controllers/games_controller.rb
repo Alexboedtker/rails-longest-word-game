4c13f77195f5e46b1d31931ea86c8b37cce2951c
Alexandra Boedtker <alexboedtker@gmail.com>
2021-04-12 16:27:06 +0200

    structure complete

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,30 @@
 class GamesController < ApplicationController
   def new
-    alphabet = ('A'..'Z').to_a
-    @letters = 10.times.map { alphabet.sample }
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
+    @word = params[:word]
+    @score = nil
+
+    if included?(@word.upcase, @letters)
+      if english_word?(@word)
+        @score = "Congratulations! ${word} is a valid English word!"
+      else
+        @score = "Sorry but ${@word} does not seem to be a valid English word..."
+      end
+    else
+      @score = "Sorry but ${@word} can't be built out of ${@letters}"
+    end
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+  def included?(attempt, grid)
+    aattempt.chars.all? { |letter| attempt.count(letter) <= grid.count(letter) }
   end
 end
